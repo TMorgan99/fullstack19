b9d28b4b9dc144c352117ac62e276dd9db6e782d
Your Name <you@example.com>
2019-10-14 05:33:35 -0700

    1.14*: anecdotes step3

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -2,16 +2,16 @@ import React, {useState} from 'react'
 import './App.css'
 
 const App = ({anecdotes}) => {
-
   const nextAnecdote = () =>
-      Math.floor( Math.random() * anecdotes.length )
+    Math.floor( Math.random() * anecdotes.length )
 
-  const [selected, setSelected] = useState( nextAnecdote )
-  const [votes, setVotes] = useState(
-    Array(anecdotes.length).fill(0)
-  )
+  // ties awarded to the leftmost
+  const trending = () => {
+    const greatestScore = Math.max.apply(null, votes)
+    return votes.indexOf(greatestScore)
+  }
 
-// vote for the 'selected'  elemwnt
+  // vote for the 'selected' elemwnt
   const vote = () => {
     const myCopy = [...votes]
     myCopy[ selected ] += 1
@@ -19,12 +19,19 @@ const App = ({anecdotes}) => {
     setVotes( myCopy )
   }
 
+  // state
+  const [selected, setSelected] = useState( nextAnecdote )
+  const [votes, setVotes] = useState(
+    Array(anecdotes.length).fill(0)
+  )
+
   return (
     <div className="App">
       <header className="App-header">
           Anecdotes
       </header>
 
+      <h3> Anecdote of the day </h3>
       <p> <cite> { anecdotes[selected] } </cite> </p>
       <p> this anecdote has {votes[selected]} votes
       <button onClick={()=>vote()} >
@@ -34,6 +41,8 @@ const App = ({anecdotes}) => {
       <button onClick={()=>setSelected(nextAnecdote)} >
         next Anecdote
       </button>
+      <h4> Trending Anecdote </h4>
+      <p> <cite> { anecdotes[trending()] } </cite> </p>
 
     </div>
   )
