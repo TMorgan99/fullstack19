90154609c048126ec6245b91431f6aaf5ba9f2dd
Your Name <you@example.com>
2019-10-14 05:02:23 -0700

    1.12*: anecdotes step1

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -2,17 +2,24 @@ import React, {useState} from 'react'
 import './App.css'
 
 const App = ({anecdotes}) => {
-  const [selected, setSelected] = useState(
-    Math.floor( Math.random() * anecdotes.length )
-  )
-  
+
+  const nextAnecdote = () =>
+      Math.floor( Math.random() * anecdotes.length )
+
+  const [selected, setSelected] = useState( nextAnecdote )
+
   return (
     <div className="App">
       <header className="App-header">
           Anecdotes
       </header>
 
-      { anecdotes[selected] }
+      <p> { anecdotes[selected] } </p>
+      <button
+          onClick={()=>setSelected(nextAnecdote)} >
+        next Anecdote
+      </button>
+
     </div>
   )
 }
