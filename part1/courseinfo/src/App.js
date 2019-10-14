d7018fb835368abc4708d728e69db4c0c45d228e
Your Name <you@example.com>
2019-10-13 21:20:34 -0700

    1.5 Clean

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -22,43 +22,6 @@ const Total = ({parts}) =>
   </p>
 
 const App = () => {
-  // const course = 'Half Stack application development'
-  // const part1 = 'Fundamentals of React'
-  // const exercises1 = 10
-  // const part2 = 'Using props to pass data'
-  // const exercises2 = 7
-  // const part3 = 'State of a component'
-  // const exercises3 = 14
-
-    // const course = 'Half Stack application development'
-    // const part1 = {
-    //   name: 'Fundamentals of React',
-    //   exercises: 10
-    // }
-    // const part2 = {
-    //   name: 'Using props to pass data',
-    //   exercises: 7
-    // }
-    // const part3 = {
-    //   name: 'State of a component',
-    //   exercises: 14
-    // }
-
-    // const course = 'Half Stack application development'
-    // const parts = [
-    //   {
-    //     name: 'Fundamentals of React',
-    //     exercises: 10
-    //   },
-    //   {
-    //     name: 'Using props to pass data',
-    //     exercises: 7
-    //   },
-    //   {
-    //     name: 'State of a component',
-    //     exercises: 14
-    //   }
-    // ]
 
     const course = {
       name: 'Half Stack application development',
@@ -86,20 +49,6 @@ const App = () => {
       </header>
 
       <div>
-      { /*
-        <h1>{course}</h1>
-        <p>
-          {part1} {exercises1}
-        </p>
-        <p>
-          {part2} {exercises2}
-        </p>
-        <p>
-          {part3} {exercises3}
-        </p>
-        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-      */ }
-
         <Header course={course.name} />
         <Content parts={course.parts}/>
         <Total  parts={course.parts}/>
