10f5ff2f3485a92275db7de28cc96edc838525b0
Your Name <you@example.com>
2019-10-13 21:16:13 -0700

    1.5: course information step5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -44,21 +44,40 @@ const App = () => {
     //   exercises: 14
     // }
 
-    const course = 'Half Stack application development'
-    const parts = [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+    // const course = 'Half Stack application development'
+    // const parts = [
+    //   {
+    //     name: 'Fundamentals of React',
+    //     exercises: 10
+    //   },
+    //   {
+    //     name: 'Using props to pass data',
+    //     exercises: 7
+    //   },
+    //   {
+    //     name: 'State of a component',
+    //     exercises: 14
+    //   }
+    // ]
+
+    const course = {
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7
+        },
+        {
+          name: 'State of a component',
+          exercises: 14
+        }
+      ]
+    }
+
 
   return (
     <div className="App">
@@ -81,9 +100,9 @@ const App = () => {
         <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
       */ }
 
-        <Header course={course} />
-        <Content parts={parts}/>
-        <Total  parts={parts}/>
+        <Header course={course.name} />
+        <Content parts={course.parts}/>
+        <Total  parts={course.parts}/>
       </div>
 
     </div>
