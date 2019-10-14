f6dba6719f756692333f317d7b901562e69a264b
Your Name <you@example.com>
2019-10-14 01:57:48 -0700

    1.12 random

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -2,9 +2,10 @@ import React, {useState} from 'react'
 import './App.css'
 
 const App = ({anecdotes}) => {
-  const [selected, setSelected] = useState(0)
-
-
+  const [selected, setSelected] = useState(
+    Math.floor( Math.random() * anecdotes.length )
+  )
+  
   return (
     <div className="App">
       <header className="App-header">
