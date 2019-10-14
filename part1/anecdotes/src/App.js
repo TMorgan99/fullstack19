f67151b274187fc7408b2ca0caee254d1b20925c
Your Name <you@example.com>
2019-10-14 01:13:38 -0700

    Anecdotes

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+          Anecdotes
+      </header>
+
+    </div>
+  )
+}
+
+export default App
