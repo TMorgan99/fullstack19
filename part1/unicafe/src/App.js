12bf318fe5b87f4fd00e5bf8dec61208a7e0533f
Your Name <you@example.com>
2019-10-13 22:45:51 -0700

    1.7: unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const all = good + neutral + bad
+  const average = ( good - bad ) / all
+  const positive = good / all * 100.0
+
   return (
     <div className="App">
       <header className="App-header">
           Unicafe
       </header>
+
+      <div>
+      <h5> give feedback </h5>
+        <button onClick={()=>setGood(good +1)}>good</button>
+        <button onClick={()=>setNeutral(neutral +1)}>neutral</button>
+        <button onClick={()=>setBad(bad +1)}>bad</button>
+      <h5> statistics </h5>
+        <p> good {good} </p>
+        <p> neutral {neutral} </p>
+        <p> bad {bad} </p>
+        <hr />
+        <p> all {all} </p>
+        <p> average {average} </p>
+        <p> positive {positive} % </p>
+      </div>
 
     </div>
   )
