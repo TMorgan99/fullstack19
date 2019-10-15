07c5e10e442b50decc7cebcc02c12d3e2412209e
Tim Morgan <tmorgan99@gmail.com>
2019-10-14 22:31:04 -0700

    Phonebook

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Phonebook
+      </header>
+    </div>
+  )
+}
+
+export default App
