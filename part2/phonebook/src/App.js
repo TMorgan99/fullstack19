f43046a0f1761d72d924729f248905f97651921c
Tim Morgan <tmorgan99@gmail.com>
2019-10-15 11:54:40 -0700

    2.6: The Phonebook Step1

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,12 +1,58 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css'
 
+const PhoneNumber = ({phoneNumber}) =>
+  <>
+    {phoneNumber.name}
+    <br />
+  </>
+
+const PhoneList = ({phoneNumbers}) =>
+  <>
+    <h2>Numbers</h2>
+    { phoneNumbers.map( phoneNumber =>
+      <PhoneNumber key={phoneNumber.name} phoneNumber={phoneNumber} />
+    ) }
+  </>
+
 const App = () => {
+  const [ phoneNumbers, setPhoneNumbers] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [ newName, setNewName ] = useState('')
+
+  const handleNameChange = (event) =>
+    setNewName(event.target.value)
+
+  const addPhoneNumber = (event) => {
+    console.log('add')
+    event.preventDefault()
+    const phoneNumberObject = {
+      name: newName,
+    }
+
+    setPhoneNumbers(phoneNumbers.concat(phoneNumberObject))
+    setNewName('')
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         Phonebook
       </header>
+
+      <form onSubmit={addPhoneNumber}>
+        <fieldset>
+          <legend>Phonebook</legend>
+          <label htmlFor="name">name</label>
+          <input name='name' value={newName} onChange={handleNameChange} />
+          <div>
+          <button type="submit">add</button>
+          </div>
+        </fieldset>
+      </form>
+      <PhoneList phoneNumbers={phoneNumbers} />
+
     </div>
   )
 }
