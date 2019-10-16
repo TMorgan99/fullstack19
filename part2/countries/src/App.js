3ed862e04c207cd2403ec5e0be028f865bdf4996
Tim Morgan <tmorgan99@gmail.com>
2019-10-15 17:30:08 -0700

    Countries App

---
+++ part2/countries/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import axios from 'axios'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+          Countries
+      </header>
+
+
+    </div>
+  )
+}
+
+export default App
