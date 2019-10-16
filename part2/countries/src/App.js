0cc8fa7c92efe72f7a8b6ea9527ff81a20e6fc78
Tim Morgan <tmorgan99@gmail.com>
2019-10-15 21:20:44 -0700

    2.13*: Data for countries, step2

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -18,8 +18,10 @@ const OneCountry = ({country}) =>
     <Weather location={country.capital}/>
   </>
 
-const Country = ({country}) =>
-  <li> {country.name} </li>
+
+const Country = ({country, setSearch}) =>
+  <li> <button onClick={()=>setSearch(country.name)}
+  > {country.name} </button> </li>
 
 const NoMatches = () =>
   <div className='none'>
@@ -36,18 +38,18 @@ const OneMatch = ({selectedCountries}) =>
     <OneCountry country={selectedCountries[0]} />
   </div>
 
-const SeletableMatches = ({selectedCountries}) =>
+const SeletableMatches = ({selectedCountries, setSearch}) =>
   <div className='selectable'>
     <h3> {selectedCountries.length} Countries Match the Search </h3>
     <ol>
       { selectedCountries.map( country =>
-        <Country key={country.name} country={country} />
+        <Country key={country.name} country={country} setSearch={setSearch} />
       )}
     </ol>
   </div>
 
-
-const Results = ({search, countries}) => {
+// select from the search, and select the rendering componemt to use
+const Results = ({search, countries, setSearch}) => {
   const searchRegEx = new RegExp(search, 'i')
 
   const selectedCountries =
@@ -63,15 +65,15 @@ const Results = ({search, countries}) => {
         return <OneMatch selectedCountries={selectedCountries} />
 
       case 2: case 3: case 4: case 5: case 6: case 7: case 8: case 9: case 10:
-        return <SeletableMatches selectedCountries={selectedCountries} />
+        return <SeletableMatches selectedCountries={selectedCountries} setSearch={setSearch} />
       default:
         return <ManyMatches selectedCountries={selectedCountries} />
     }
 
-// not reachable ..
-    return (
-      <> {selectedCountries.length} countries leftover </>
-    )
+  // not reachable ..but good for debug
+    // return (
+    //   <> {selectedCountries.length} countries leftover </>
+    // )
 }
 
 // =====================================================================
@@ -82,6 +84,10 @@ const App = (props) => {
   const handleChangeSearch = (event) => {
     setSearch(event.target.value)
   }
+
+// future warning!
+  // A cookie associated with a cross-site resource at http://restcountries.eu/ was set without the `SameSite` attribute. A future release of Chrome will only deliver cookies with cross-site requests if they are set with `SameSite=None` and `Secure`. You can review cookies in developer tools under Application>Storage>Cookies and see more details at https://www.chromestatus.com/feature/5088147346030592 and https://www.chromestatus.com/feature/5633521622188032.
+
   // ===== HOOKS ======================================================
     // console.log('*** firing load_countries Effect')
   useEffect(()=>{
@@ -105,8 +111,9 @@ const App = (props) => {
         <fieldset>
           <label htmlFor="search"> Find Countries: </label>
           <input id="search" value={search} onChange={handleChangeSearch} />
+          <button id="clear" onClick={()=>setSearch('')} > x </button>
         </fieldset>
-        < Results countries={countries} search={search} />
+        < Results countries={countries} search={search} setSearch={setSearch} />
       </>
       }
     </div>
