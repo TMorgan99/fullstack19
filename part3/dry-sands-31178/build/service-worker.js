1382b04abcd42a8886ff7a50d7811c0cdc3ec8e6
Tim Morgan <tmorgan99@gmail.com>
2019-11-15 15:14:45 -0800

    handbuild

--- part3/dry-sands-31178/build/service-worker.js
+++ part3/dry-sands-31178/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.39c286f704f5023e23e44877780f6fd0.js"
+  "/precache-manifest.b5fd56a3bbc6f43c78b0411efab4c30a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
--- part3/dry-sands-31178/setpw.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-DB=phonebook-app
-MONGO_PW=s5Ynl9jA4LGjQsgl
-
-export MONGODB_URI=mongodb://localhost/$DB
-export MONGODB_URI=mongodb+srv://fullstack19:$MONGO_PW@cluster0-im6gb.azure.mongodb.net/$DB?retryWrites=true&w=majority
-
-
